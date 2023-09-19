631bab738849ffdb56839fa16d4920e7995674f6
ojo gbotemi <hommzmum@gmail.com>
2023-09-19 06:33:21 +0100

    project completed

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,11 +1,14 @@
-import './header.css'
+import './header.css';
+import { Link } from 'react-router-dom';
 function Header(props) {
-  return (
-      <header class="header">
-        <h3 className='header_message'>{props.message}</h3>
-              <a href="" className="button1" target="_blank" rel="noopener" onClick={props.onClick}>{props.status}</a>
-      </header>
-  )
+    return (
+        <header class="header">
+            <h3 className='header_message'>{props.message}</h3>
+            <Link to={'/signin'}>
+                <button href="#" className="button1 button" target="_blank" rel="noopener" onClick={props.onClick}>{props.status}</button>
+            </Link>
+        </header>
+    )
 }
 
 export default Header;
